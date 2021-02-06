65cd6a2497a44aa51d3991ea342a94feae627b2d
Sergio Rios <sergiorios@MacBook-de-Sergio.local>
2021-02-06 15:01:55 -0600

    bloqueos

--- src/resolvers/bloqueos.js
+++ src/resolvers/bloqueos.js
@@ -1,3 +1,5 @@
+const { db } = require("../pgAdaptor");
+
 const resolvers = {
   Mutation: {
     createBloqueo: (_, { input }, { db }) =>
@@ -7,6 +9,11 @@ const resolvers = {
     deleteBloqueo: (_, { id }, { db }) =>
       db.one("DELETE FROM bloqueos WHERE id=$1 RETURNING id", id)
   },
+  Query:{
+    bloqueos: (_,{id},{db}) =>{
+      return db.any("SELECT * FROM bloqueos WHERE usuario_id =$1 AND f_termino > CURRENT_TIMESTAMP",id);
+    }
+  }
 };
 
 module.exports = resolvers;
